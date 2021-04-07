633138822a3f4bb986e6e26c1f2dee54faf7d02d
Mathias Van de Pol <vandem17@cronos.be>
2021-04-07 16:41:04 +0200

    set version

--- DSC/timezone/localhost.ps1
+++ DSC/timezone/localhost.ps1
@@ -1,5 +1,5 @@
 configuration timezone {
-    Import-DscResource -ModuleName ComputerManagementDsc
+    Import-DscResource -ModuleName ComputerManagementDsc -ModuleVersion 5.0.0.0
 
     TimeZone westeustandard {
         IsSingleInstance = 'Yes'
